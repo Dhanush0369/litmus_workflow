e68545f22360a5864be187d66adc106269a964b1
Dhanush0369 <dhanush789245@gmail.com>
2024-05-20 12:01:14 +0530

    Added chaoscenter tests
    Signed-off-by: Dhanush0369 <dhanush789245@gmail.com>

---
+++ chaoscenter/cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://192.168.49.2:32479',
+    requestTimeout: 60000,
+    responseTimeout: 60000
+  },
+  env: {
+    username: 'admin',
+    password: 'litmus'
+  },
+});
